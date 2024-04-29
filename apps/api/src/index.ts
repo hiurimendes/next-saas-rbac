56422d29ae8c20a2b6697c88af4df935f3e311cb
Hiuri Mendes <hiurimendes@gmail.com>
2024-04-29 08:36:06 -0300

    add `readme` file

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,8 @@
-import { defineAbilityFor } from '@saas/auth'
+import { defineAbilityFor, projectSchema } from '@saas/auth'
 
-const ability = defineAbilityFor({ role: 'ADMIN' })
+const ability = defineAbilityFor({ role: 'ADMIN', id: 'user-id' })
+const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
 
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
-const userCanDeleteOtherUsers = ability.can('delete', 'User')
-
-console.log(userCanInviteSomeoneElse)
-console.log(userCanDeleteOtherUsers)
+console.log(ability.can('get', 'Billing'))
+console.log(ability.can('create', 'Invite'))
+console.log(ability.can('delete', project))
