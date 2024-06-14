1439074fba359fad05e161aa2804fcf22fc45174
Hiuri Mendes <hiurimendes@gmail.com>
2024-06-14 11:05:00 -0300

    Parallel routes no Next.js

--- apps/web/src/app/(app)/org/[slug]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -1,7 +1,10 @@
+import { Header } from '@/components/header'
+
 export default async function Projects() {
   return (
-    <div className="py-4 ">
-      <main></main>
+    <div className="py-4 space-y-4">
+      <Header />
+      <main className="mx-auto w-full max-w-[1200px] space-y-4">Projects</main>
     </div>
   )
 }
