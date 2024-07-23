f35a94efb0e7b9262b761e2e0c921d1e2d510c6c
Hiuri Mendes <hiurimendes@gmail.com>
2024-07-23 15:09:50 -0300

    Edição da organização

--- apps/web/src/app/(app)/org/[slug]/projects/[project]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/projects/[project]/page.tsx
@@ -1,7 +1,7 @@
 export default async function Project() {
   return (
     <div className="py-4 space-y-4">
-      <main className="mx-auto w-full max-w-[1200px] space-y-4">Project</main>
+      <h1 className="text-2xl font-bold">Projects</h1>
     </div>
   )
 }
